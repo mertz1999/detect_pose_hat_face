e8cfcb2d8e48504bdaffd06a110143ee88e2c607
Reza Tanakizadeh <reza.tz780210@gmail.com>
2023-12-26 15:43:01 +0000

    (REFACTOR)

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 import os
 import torch
+import time
+import tensorflow as tf
 
 # Process class instance
 main_process = MainProcess(hat_model='./models/hat_detection.pt', 
@@ -12,7 +14,8 @@ main_process = MainProcess(hat_model='./models/hat_detection.pt',
 
 # Main loop
 video_path = './inputs/2.mp4'
-cap        = cv2.VideoCapture(video_path)
+# cap        = cv2.VideoCapture(video_path)
+cap        = cv2.VideoCapture(0)
 width      = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height     = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps        = int(cap.get(cv2.CAP_PROP_FPS))
@@ -22,22 +25,35 @@ output_path = './result.mp4'
 out         = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
 count = -1
-while cap.isOpened():
-    count += 1
-    ret, img = cap.read()
-    if not ret:
-        break
-    img_copy = img.copy()
+with tf.device(tf.DeviceSpec(device_type="CPU", device_index=0)):
+    while cap.isOpened():
+        count += 1
+        ret, img = cap.read()
+        if not ret:
+            break
+        img_copy = img.copy()
+        cv2.imshow('frame', img_copy)
 
-    face_box    = main_process.face_model(img_copy)   
-    face_box    = face_box[0].boxes.xyxy.detach().cpu().numpy()
+        start_time = time.time()
+        face_result    = main_process.face_model(img_copy)   
+        face_box    = face_result[0].boxes.xyxy.detach().cpu().numpy()
 
-    persons_box = main_process.pose_model(img_copy)
-    persons_box = persons_box[0].boxes.xyxy.detach().cpu().numpy()
-    pose_lands  = persons_box[0].keypoints.xy.detach().cpu().numpy()
+        pose_result = main_process.pose_model(img_copy)
+        persons_box = pose_result[0].boxes.xyxy.detach().cpu().numpy()
+        pose_lands  = pose_result[0].keypoints.xy.detach().cpu().numpy()
 
-    img_copy = main_process.draw_person(img_copy, persons_box, face_box)
-    img_copy = main_process.draw_face(img_copy, face_box)
-    img_copy = main_process.draw_pose(img_copy, pose_lands)
+        img_copy = main_process.draw_person(img_copy, persons_box, face_box)
+        img_copy = main_process.draw_face(img_copy, face_box)
+        img_copy = main_process.draw_pose(img_copy, pose_lands)
 
-    break
+        print('\nTotal secounds : ', time.time() - start_time,'\n')
+
+        cv2.imwrite('./temp.png', img_copy)
+
+        cv2.imshow('frame', img_copy)  
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+        # break
+        out.write(img_copy)
+
+out.release()
